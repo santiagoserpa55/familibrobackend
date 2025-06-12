2b81495b3fb87dca7a4947b74e91c9b3e7bf8af9
Santiago Serpa <santiagoserpa902@gmail.com>
2025-06-12 10:17:58 -0500

    ajuste url a vercel

--- src/main/java/com/epsfamiliar/familibro/controller/DataController.java
+++ src/main/java/com/epsfamiliar/familibro/controller/DataController.java
@@ -11,7 +11,7 @@ import com.epsfamiliar.familibro.service.DataService;
 
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://familibroapp.vercel.app")
 @RestController
 public class DataController {
 
--- src/main/java/com/epsfamiliar/familibro/controller/MedicamentosController.java
+++ src/main/java/com/epsfamiliar/familibro/controller/MedicamentosController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.epsfamiliar.familibro.model.MedicamentosModel;
 import com.epsfamiliar.familibro.service.MedicamentosService;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://familibroapp.vercel.app")
 @RestController
 public class MedicamentosController {
 	private MedicamentosService medicamentosService;
